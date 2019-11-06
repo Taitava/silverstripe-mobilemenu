48b1b8f760450bc360c10af4213a8ce7544de1fc
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2019-11-06 11:42:09 +0200

    Support for PHP 7.2

--- code/MobileMenu.php
+++ code/MobileMenu.php
@@ -1,12 +1,15 @@
 <?php
 
+// Support SilverStripe versions lower than 3.7:
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 /**
  * Class MobileMenu
  *
  * This class is used just for configuration.
  *
  */
-class MobileMenu extends Object
+class MobileMenu extends SS_Object // If SilverStripe version in lower than 3.7, SS_Object will be an alias for Object class. In SS 3.7 SS_Object is a real class.
 {
 	/**
 	 * If the viewport is narrower than this value (in pixels), show the mobile menu button. Otherwise hide it.
